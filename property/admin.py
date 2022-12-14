0530ba4b5491a72f07c43aece8a2f13a04684b27
beltagy <moh.bell@crowdbotics.com>
2022-12-14 18:13:55 +0200

    add admin tenaants for each property

--- property/admin.py
+++ property/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class TenantInline(admin.TabularInline):
+    model = Tenant
+
 class AdminTenant(admin.ModelAdmin):
     model = Tenant
-    pass
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+     inlines = [TenantInline]
+
 
 admin.site.register(Tenant, AdminTenant)
-admin.site.register(Property)
+
 admin.site.register(PropertyImage)
 admin.site.register(PropertyType)
 
