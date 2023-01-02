9c4193221b7d3254a64c1b619aaec50761db56a3
beltagy <moh.bell@crowdbotics.com>
2023-01-02 13:20:21 +0200

    add inline mainteance request align with property model

--- property/admin.py
+++ property/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
 from .models import *
+from mrequest.models import MRequest
 # Register your models here.
 class TenantInline(admin.TabularInline):
     model = Tenant
+    extra =1
 
 class InspectionInline(admin.TabularInline):
     model = InspectionSchedule
 
 class AdminTenant(admin.ModelAdmin):
     model = Tenant
-
+    extra =1
 class DiscrepancyInline(admin.TabularInline):
     model = Discrepancy
+    extra=1
+
+class InlineMRequest(admin.TabularInline):
+    model = MRequest 
+    extra =1
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
      search_fields = ["name", ]
-     inlines = [TenantInline, InspectionInline, DiscrepancyInline]
+     inlines = [TenantInline, InspectionInline, DiscrepancyInline, InlineMRequest]
 
 
 
