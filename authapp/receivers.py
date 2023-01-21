349796e2dcf1264c7e512ad8cad05f7a425d2ecb
beltagy <moh.bell@crowdbotics.com>
2023-01-21 08:59:17 +0200

    add template for signup otp

--- authapp/receivers.py
+++ authapp/receivers.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.mail import send_mail
 from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 
 
@@ -12,9 +14,27 @@ def send_verification_email(sender, instance, **kwargs):
     user = instance.user
     to_ = user.email
     from_ = settings.DEFAULT_FROM_EMAIL
+    context = {
+        "verification_code":instance.code
+    }
     message = "Your verification code is {}".format(instance.code)
+    title = f"Inspection360 Account Verification"
+    email_html_message = render_to_string('email/signup_verification_email.html', context)
+    email_plaintext_message =message
+    
     try:
-        send_mail('Inspection', message, from_, [to_], fail_silently=False)
+        msg = EmailMultiAlternatives(
+        # title:
+        title,
+        # message:
+         email_plaintext_message,
+        # from:
+        from_,
+        # to:
+        [to_]
+        )
+        msg.attach_alternative(email_html_message, "text/html")
+        msg.send()
     except:
         pass
 
