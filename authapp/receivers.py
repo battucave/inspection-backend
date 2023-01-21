97c46001b2a37fb89e8c3afbd1db03b9df2fc535
beltagy <moh.bell@crowdbotics.com>
2023-01-21 08:20:25 +0200

    fix wronge from email to send otp while signup

--- authapp/receivers.py
+++ authapp/receivers.py
@@ -3,6 +3,7 @@ from authapp.models import User,VerificationCode
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.mail import send_mail
+from django.conf import settings
 
 
 
@@ -10,7 +11,7 @@ from django.core.mail import send_mail
 def send_verification_email(sender, instance, **kwargs):
     user = instance.user
     to_ = user.email
-    from_ = 'this@inspection.com'
+    from_ = settings.DEFAULT_FROM_EMAIL
     message = "Your verification code is {}".format(instance.code)
     try:
         send_mail('Inspection', message, from_, [to_], fail_silently=False)
