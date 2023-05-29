801272bec0ca2641efed6a60313eaf3925c9a784
Huzaifa Ahmed <ha300101@hotmail.com>
2023-05-29 21:11:35 +0500

    migration fixed

--- report/migrations/0009_report_property.py
+++ report/migrations/0009_report_property.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0032_alter_inspectionschedule_last_triggered_and_more'),
         ('report', '0008_report_created_at'),
     ]
 
