9a6d943f5a03949c2326a4bbd3d9f86b30bb898c
Huzaifa Ahmed <ha300101@hotmail.com>
2023-05-31 00:43:43 +0500

    Fixed import

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,4 @@
+import random
 from .models import User,VerificationCode
 from .serializers import UserCreateSerializer, PasswordResetTokenSerializer
 from rest_framework.decorators import api_view, permission_classes
